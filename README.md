d92218007d72c8eb8c4fd448b9a0633daaaef9b7
Magda Mollo <magda.mollo@finanzero.com.br>
2024-05-14 18:10:58 -0300

    alterações e adição de novas telas

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ PORT=3000
 CONNECTION_STRING=mongodb://localhost:27017
 ```
 
+Rodar Localmente: 
+1-Verificar a versao node instalada na maquina(usar a versao v20.11.1)
+2-Instalar as dependencias nas pastas do cliente e no server,usando o comando npm i ou npm install
+3-Rodar o comando npm run start
+
 
 
 
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,21 +1,26 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import '../css/login.css'
+import { ButtonGroup } from 'react-bootstrap'
+
 export default function Login() {
     return (
         <div id="login-raiz">
             <div id="login-container">
-            <h1>Sistema de Controle de Chamados</h1>
+            <h1>Sistema Controle de Chamados</h1>
                 <Form id="login-form">
                     <Form.Group className="mb-3">
-                        <Form.Label>Nome de usuário</Form.Label>
+                        <Form.Label>Email:</Form.Label>
                         <Form.Control type="text" />
                     </Form.Group>
                     <Form.Group className="mb-3">
-                        <Form.Label>Senha</Form.Label>
+                        <Form.Label>Senha:</Form.Label>
                         <Form.Control type="password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit">Entrar</Button>
+                    <ButtonGroup>
+                        <Button variant="secondary" type="submit" size='lg' href="/recupera-senha">Esqueci Senha</Button>
+                        <Button variant="primary" type="submit" size='lg'>Entrar</Button>
+                    </ButtonGroup>   
                 </Form>
             </div>
         </div>
