795462933716192b4b33de13da19b17ec0d14be0
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-10-31 14:57:42 -0300

    Add validação dos campos

--- client/src/pages/atendentes/cadastrar_atendente.js
+++ client/src/pages/atendentes/cadastrar_atendente.js
@@ -5,6 +5,12 @@ import Row from "react-bootstrap/Row";
 import '../../css/atendentes/cadatendentes.css'
 import { ButtonGroup } from 'react-bootstrap';
 import {useState} from "react";
+import {
+    CadastroPessoaFisica as CPF,
+    TelefoneFixo as TelFixo,
+    TelefoneCelular as TelCel,
+    Validar
+} from '../validacao'
 
 export default function Cadastrar_atendente() {
 
@@ -16,8 +22,173 @@ export default function Cadastrar_atendente() {
     const [dataContrato, setDataContrato] = useState('');
     const [senha, setSenha] = useState('');
 
+    const [nomeError, setNomeError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [cpfError, setCpfError] = useState('');
+    const [telefoneError, setTelefoneError] = useState('');
+    const [celularError, setCelularError] = useState('');
+    const [dataContratoError, setDataContratoError] = useState('');
+    const [senhaError, setSenhaError] = useState('');
+
+    //#region HandlingThings
+    const handleKeyDownCpf= (e) => {
+        if(Validar.isCaracterDeControle(e.key)) return;
+
+        if(!CPF.isValidKey(e.key)){
+            e.preventDefault();
+        }else if(!CPF.hasNextKey(e.target.value)){
+            e.preventDefault();
+        }else{
+            e.target.value += CPF.getNextFormatKey(e.target.value);
+        }
+    }
+
+    const handleKeyDownTelFixo = (e) => {
+        if(Validar.isCaracterDeControle(e.key)) return;
+
+        if(!TelFixo.isValidKey(e.key)){
+            e.preventDefault();
+        }else if(!TelFixo.hasNextKey(e.target.value)){
+            e.preventDefault();
+        }else{
+            e.target.value += TelFixo.getNextFormatKey(e.target.value);
+        }
+    }
+
+    const handleKeyDownTelCel = (e) => {
+        if(Validar.isCaracterDeControle(e.key)) return;
+
+        if(!TelCel.isValidKey(e.key)){
+            e.preventDefault();
+        }else if(!TelCel.hasNextKey(e.target.value)){
+            e.preventDefault();
+        }else{
+            e.target.value += TelCel.getNextFormatKey(e.target.value);
+        }
+    }
+
+    const handleKeyDownNome = (e) => {
+        if(Validar.isCaracterDeControle(e.key)) return;
+
+        if(!Validar.isNomeKey(e.key)){
+            e.preventDefault();
+        }
+    }
+
+    const handleKeyDownEmail = (e) => {
+        if(Validar.isCaracterDeControle(e.key)) return;
+
+        if(!Validar.isEmailKey(e.key)){
+            e.preventDefault();
+        }
+    }
+
+    //#endregion
+
+    //#region Validating
+    const validarNome = (nome) => {
+        if(Validar.isNome(nome)){
+            setNomeError('');
+            setNome(nome);
+        }else if(Validar.isNotEmptyStr(nome)){
+            setNomeError('Inválido');
+        }else{
+            setNomeError('Obrigatório');
+        }
+    }
+
+    const validarCPF = (cpf) => {
+        if(cpf === ""){
+            setCpfError("Obrigatório!");
+        }
+        else if(CPF.hasNextKey(cpf)){
+            setCpfError("Incompleto!");
+        }else if(!CPF.isNumericValid(cpf)){
+            setCpfError("Inválido!");
+        }else{
+            setCpfError("");
+            setCpf(CPF.getOnlyDigits(cpf));
+        }
+    }
+
+    const validarEmail = (email) => {
+        if(!Validar.isNotEmptyStr(email)){
+            setEmailError("Obrigatório!")
+        }else if(!Validar.isEmail(email)){
+            setEmailError("Inválido.")
+        }else{
+            setEmail(email);
+            setEmailError("");
+        }
+    }
+
+    const validarTel = (telefone) => {
+        if(TelFixo.isFormatValid(telefone)){
+            setTelefone(telefone);
+            setTelefoneError("");
+        }else if(!Validar.isNotEmptyStr(telefone)){
+            setTelefoneError("Obrigatório!");
+        }else{
+            setTelefoneError("Incompleto!");
+        }
+    }
+
+    const validarCel = (celular) => {
+        if(TelCel.isFormatValid(celular)){
+            setCelular(celular);
+            setCelularError("");
+        }else if(!Validar.isNotEmptyStr(celular)){
+            setCelularError("Obrigatório!");
+        }else{
+            setCelularError("Incompleto!");
+        }
+    }
+
+    const validarSenha = (senha) => {
+        if(Validar.isSenha(senha)){
+            setSenhaError("");
+            setSenha(senha);
+        }else if(!Validar.isNotEmptyStr(senha)){
+            setSenhaError("Obrigatório!");
+        }else{
+            setSenhaError("Incompleto!");
+        }
+    }
+
+    const validarDataContrato = (data) => {
+        if(Validar.isNotEmptyStr(data)){
+            setDataContrato(data);
+        }else{
+            setDataContratoError("Obrigatório");
+        }
+    }
+    //#endregion
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        const tudoValido = 
+            nomeError
+            && emailError
+            && cpfError
+            && telefoneError
+            && celularError
+            && dataContratoError
+            && senhaError;
+        
+        if(tudoValido === false){
+            console.error("Formulário inválido:");
+            console.error(
+                `nome: ${nomeError}
+                emailError: ${emailError}
+                cpfError: ${cpfError}
+                telefoneError: ${telefoneError}
+                celularError: ${celularError}
+                dataContratoError: ${dataContratoError}
+                senhaError: ${senhaError}` 
+            );
+            return;
+        }
+
         const dados = {
             "nome": nome,
             "cpf": cpf,
@@ -51,31 +222,100 @@ export default function Cadastrar_atendente() {
             <Form id="cadatendente-form" onSubmit={handleSubmit}>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Nome Completo</Form.Label>
-                    <Col sm={10}><Form.Control required onChange={(e) => setNome(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            onChange={(e) => validarNome(e.target.value)} 
+                            onKeyDown={(e) => handleKeyDownNome(e)}
+                            isInvalid={nomeError}
+                            feedback={nomeError}
+                            feedbacktype={"invalid"}
+                            type="text"/>
+                    </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>CPF</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: 000.000.000-00" maxLength={14} onChange={(e) => setCpf(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="cpf" 
+                            required 
+                            placeholder="000.000.000-00" 
+                            maxLength={14} 
+                            onChange={(e) => validarCPF(e.target.value)} 
+                            onKeyDown={(e)=> handleKeyDownCpf(e)}
+                            isInvalid={cpfError}
+                            feedback={cpfError}
+                            feedbacktype={"invalid"}
+                            type="text"/>
+                    </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Telefone</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: (00) 0000-0000" maxLength={14} onChange={(e) => setTelefone(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="telFixo" 
+                            required 
+                            placeholder="(00) 0000-0000" 
+                            maxLength={14} 
+                            onChange={(e) => validarTel(e.target.value)} 
+                            onKeyDown={(e) => handleKeyDownTelFixo(e)}
+                            isInvalid={telefoneError}
+                            feedback={telefoneError}
+                            feedbacktype={"invalid"}
+                            type="text"/>
+                        </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Celular</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: (00) 00000-0000" maxLength={15} onChange={(e) => setCelular(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="telCel"
+                            required 
+                            placeholder="(00) 00000-0000" 
+                            maxLength={15} 
+                            onChange={(e) => validarCel(e.target.value)} 
+                            onKeyDown={(e) => handleKeyDownTelCel(e)}
+                            isInvalid={celularError}
+                            feedback={celularError}
+                            feedbacktype={"invalid"}
+                            type="text"/>
+                        </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Email</Form.Label>
-                    <Col sm={10}><Form.Control  required  placeholder="Ex.: exemplo@email.com" onChange={(e) => setEmail(e.target.value)} type="email"/></Col>
+                    <Col sm={10}>
+                        <Form.Control  
+                            required  
+                            placeholder="exemplo@email.com" 
+                            onChange={(e) => validarEmail(e.target.value)}
+                            onKeyDown={(e) => handleKeyDownEmail(e)}
+                            isInvalid={emailError}
+                            feedback={emailError}
+                            feedbacktype={"invalid"}
+                            type="email"/></Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Senha</Form.Label>
-                    <Col sm={10}><Form.Control required maxLength={8} onChange={(e) => setSenha(e.target.value)} type="password" /></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            maxLength={8} 
+                            onChange={(e) => validarSenha(e.target.value)}
+                            isInvalid={senhaError}
+                            feedback={senhaError}
+                            feedbacktype={"invalid"}
+                            type="password" /></Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Data de contrato</Form.Label>
-                    <Col sm={10}><Form.Control required onChange={(e) => setDataContrato(e.target.value)} type="datetime-local"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            onChange={(e) => validarDataContrato(e.target.value)}
+                            isInvalid={dataContratoError}
+                            feedback={dataContratoError}
+                            feedbacktype={"invalid"}
+                            type="datetime-local"/></Col>
                 </Form.Group>
 
                 <ButtonGroup>
