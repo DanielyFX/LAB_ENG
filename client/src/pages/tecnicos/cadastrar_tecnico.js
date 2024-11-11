0334ef956648deb5d08b28df7a1ac9dfc90dab0c
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-11-11 13:55:14 -0300

    Add validação e tratamento de erros a página cad técnico

--- client/src/pages/tecnicos/cadastrar_tecnico.js
+++ client/src/pages/tecnicos/cadastrar_tecnico.js
@@ -3,8 +3,10 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import '../../css/tecnicos/cadtecnicos.css'
-import {useState} from "react";
+import { useState } from "react";
 import { ButtonGroup } from "react-bootstrap";
+import { Validar } from '../validacao';
+import Alert from 'react-bootstrap/Alert';
 
 export default function Cadastrar_tecnico() {
 
@@ -16,8 +18,32 @@ export default function Cadastrar_tecnico() {
     const [dataContrato, setDataContrato] = useState('');
     const [senha, setSenha] = useState('');
 
+    const [nomeError, setNomeError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [cpfError, setCpfError] = useState('');
+    const [telefoneError, setTelefoneError] = useState('');
+    const [celularError, setCelularError] = useState('');
+    const [dataContratoError, setDataContratoError] = useState('');
+    const [senhaError, setSenhaError] = useState('');
+
+    const [showAlert, setShowAlert] = useState(false);
+    const [msgAlert, setMsgAlert] = useState('');
+    const [typeAlert, setTypeAlert] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        const tudoValido = 
+            !nomeError
+            && !emailError
+            && !cpfError
+            && !telefoneError
+            && !celularError
+            && !dataContratoError
+            && !senhaError;
+
+        if(tudoValido === false) return;
+
         const dados = {
             "nome": nome,
             "cpf": cpf,
@@ -35,55 +61,159 @@ export default function Cadastrar_tecnico() {
             body: JSON.stringify(dados),
             mode: 'cors'
         })
-            .then((resultado) => resultado.json())
-            .then((response) => {
-                //console.log(response)
-                if(response.success) {
-                    alert("Técnico Cadastrado com Sucesso!")
-                    window.location.reload()
-                } else {
-                    alert("Erro ao cadastrar o técnico!")
-                }
-            })
+        .then((resultado) => resultado.json())
+        .then((response) => {
+            if(response.success) {
+                setShowAlert(true);
+                setMsgAlert(`Técnico ${dados.nome}: Cadastrado com Sucesso!`);
+                setTypeAlert("success");
+                setTimeout(() => {
+                    window.location.reload();
+                }, 2000);
+            } else {
+                setShowAlert(true);
+                setMsgAlert(`Erro ao cadastrar! CPF ${Validar.CPF.getFormated(dados.cpf)} já cadastrado.`);
+                setTypeAlert("info");
+            }
+        })
+        .catch((err) => {
+            setShowAlert(true);
+            setTypeAlert("danger");
+            if(err instanceof TypeError && err.message === "Failed to fetch")
+                setMsgAlert(`Erro: Verifique sua conexão com a internet (${err.message}).`);
+            else
+                setMsgAlert(`Erro: ${err.message}`);
+        })
     }
 
     return (
         <div id="cadtecnico-main">
+            <Alert 
+                variant={typeAlert} 
+                show={showAlert} 
+                onClose={() => setShowAlert(false)} 
+                dismissible
+            >
+                <strong><p>{msgAlert}</p></strong>
+            </Alert>
             <Form id="cadtecnico-form" onSubmit={handleSubmit}>
-                <Form.Group as={Row} className="mb-3">
+            <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Nome Completo</Form.Label>
-                    <Col sm={10}><Form.Control required onChange={(e) => setNome(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            onChange={(e) => Validar.NomePessoa.handleOnChange(e.target.value, setNome, setNomeError)} 
+                            onKeyDown={(e) => Validar.NomePessoa.handleKeyDown(e)}
+                            isInvalid={nomeError}
+                            type="text"/>
+                        <Form.Control.Feedback type="invalid">
+                            {nomeError}
+                        </Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>CPF</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: 000.000.000-00" maxLength={14} onChange={(e) => setCpf(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="cpf" 
+                            required 
+                            placeholder="000.000.000-00" 
+                            maxLength={14} 
+                            onChange={(e) => Validar.CPF.handleOnChange(e.target.value, setCpf, setCpfError)} 
+                            onKeyDown={(e)=> Validar.CPF.handleKeyDown(e)}
+                            isInvalid={cpfError}
+                            type="text"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {cpfError}
+                        </Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Telefone</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: (00) 0000-0000" maxLength={14} onChange={(e) => setTelefone(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="telFixo" 
+                            required 
+                            placeholder="(00) 0000-0000" 
+                            maxLength={14} 
+                            onChange={(e) => Validar.TelFixo.handleOnChange(e.target.value, setTelefone, setTelefoneError)} 
+                            onKeyDown={(e) => Validar.TelFixo.handleKeyDown(e)}
+                            isInvalid={telefoneError}
+                            type="text"/>
+                        <Form.Control.Feedback type="invalid">
+                            {telefoneError}
+                        </Form.Control.Feedback>
+                        </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Celular</Form.Label>
-                    <Col sm={10}><Form.Control required placeholder="Ex.: (00) 00000-0000" maxLength={15} onChange={(e) => setCelular(e.target.value)} type="text"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            id="telCel"
+                            required 
+                            placeholder="(00) 00000-0000" 
+                            maxLength={15} 
+                            onChange={(e) => Validar.TelCel.handleOnChange(e.target.value, setCelular, setCelularError)} 
+                            onKeyDown={(e) => Validar.TelCel.handleKeyDown(e)}
+                            isInvalid={celularError}
+                            type="text"/>
+                        <Form.Control.Feedback type="invalid">
+                            {celularError}
+                        </Form.Control.Feedback>
+                        </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Email</Form.Label>
-                    <Col sm={10}><Form.Control  required  placeholder="Ex.: exemplo@email.com" onChange={(e) => setEmail(e.target.value)} type="email"/></Col>
+                    <Col sm={10}>
+                        <Form.Control  
+                            required  
+                            placeholder="exemplo@email.com" 
+                            onChange={(e) => Validar.Email.handleOnChange(e.target.value, setEmail, setEmailError)}
+                            onKeyDown={(e) => Validar.Email.handleKeyDown(e)}
+                            isInvalid={emailError}
+                            type="email"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {emailError}
+                        </Form.Control.Feedback>
+                        </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Senha</Form.Label>
-                    <Col sm={10}><Form.Control required maxLength={8} onChange={(e) => setSenha(e.target.value)} type="password" /></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            maxLength={8} 
+                            onChange={(e) => Validar.Senha.handleOnChange(e.target.value, setSenha, setSenhaError)}
+                            isInvalid={senhaError}
+                            type="password" 
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {senhaError}
+                        </Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
                 <Form.Group as={Row} className="mb-3">
                     <Form.Label column sm={2}>Data de contrato</Form.Label>
-                    <Col sm={10}><Form.Control required onChange={(e) => setDataContrato(e.target.value)} type="datetime-local"/></Col>
+                    <Col sm={10}>
+                        <Form.Control 
+                            required 
+                            onChange={(e) => Validar.DataContrato.handleOnChange(e.target.value, setDataContrato, setDataContratoError)}
+                            isInvalid={dataContratoError}
+                            max={Validar.DataContrato.TodayHTMLDatetimeLocalFormat} 
+                            type="datetime-local"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {dataContratoError}
+                        </Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
 
                 <ButtonGroup>
                     <Button variant="primary" type="submit">Cadastrar</Button>
                     <Button variant="secondary" href="/inicio">Cancelar</Button>
                 </ButtonGroup>
-
             </Form>
         </div>
     )
