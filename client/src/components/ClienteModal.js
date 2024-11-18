4a3b7963c73dbf146233fc3fae97cf80881edcab
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-11-18 11:41:10 -0300

    Add validação em cliente modal e consulta

--- client/src/components/ClienteModal.js
+++ client/src/components/ClienteModal.js
@@ -3,17 +3,57 @@ import Modal from 'react-bootstrap/Modal';
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useState, useEffect } from 'react';
+import {
+    CadastroPessoaFisica as CPF,
+    CadastroNacionalPessoaJuridica as CNPJ,
+    TelefoneCelular as TelCel,
+    TelefoneFixo as TelFixo,
+    CEP,
+    Validar
+} from '../pages/validacao';
 
 function ClienteModal(props) {
 
     const {handleClose, cliente, onHide} = props;
+    const {setShowAlert, setMsgAlert, setTypeAlert} = props;
+
+    const [telefoneError, setTelefoneError] = useState('');
+    const [celularError, setCelularError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [cepError, setCepError] = useState('');
+    
+    const [telefone, setTelefone] = useState(cliente.telefone);
+    const [celular, setCelular] = useState(cliente.celular);
+    const [email, setEmail] = useState(cliente.email);
+    const [cep, setCep] = useState(cliente.cep);
+    const [rua, setRua] = useState(cliente.rua || '');
+    const [cidade, setCidade] = useState(cliente.cidade || '');
+    const [bairro, setBairro] = useState(cliente.bairro || '');
+
+
+    useEffect(()=>{
+        CEP.getDataFrom(cep).then((data) => {
+            setBairro(data.bairro);
+            setCidade(data.estado);
+            setRua(data.logradouro);
+        }).catch((err) =>{
+            setCepError(err);
+        })
+    }, [cep])
+    
+    
     let dados_novos = {
         "nome": cliente.nome,
         "documento": cliente.documento,
-        "telefone": cliente.telefone,
-        "celular": cliente.celular,
-        "cep": cliente.cep,
-        "rua": cliente.rua,
+        "telefone": telefone,
+        "celular": celular,
+        "email": email,
+        "cep": cep,
+        "bairro": bairro,
+        "cidade": cidade,
+        "numero": cliente.numero,
+        "rua": rua,
         "_id": cliente._id,
     }
 
@@ -38,8 +78,37 @@ function ClienteModal(props) {
             body: JSON.stringify(body),
             mode: 'cors'
         })
-            .then((resultado) => resultado.json())
-            .then((response) => {/*console.log(response)*/})
+        .then((resultado) => resultado.json())
+        .then((response) => {
+            if(response.success){
+                handleClose();
+                if(setShowAlert && setMsgAlert && setTypeAlert){
+                    setShowAlert(true);
+                    setMsgAlert(`Alterações realizadas com sucesso`);
+                    setTypeAlert("success");
+                }
+            }else{
+                if(setShowAlert && setMsgAlert && setTypeAlert){
+                    setShowAlert(true);
+                    setMsgAlert(`Não foi possível fazer a atualização do cliente`);
+                    setTypeAlert("info");
+                }
+            }
+        })
+        .catch((err) =>{
+            if(setShowAlert && setMsgAlert && setTypeAlert){
+                setShowAlert(true);
+                setTypeAlert('danger');
+                if(err instanceof TypeError && err.message === "Failed to fetch")
+                    setMsgAlert(`Erro: Verifique sua conexão com a internet (${err.message}).`);
+                else
+                    setMsgAlert(`Erro: ${err.message}`);
+            }else{
+                console.error(err);
+            }
+        });
+
+
         setTimeout(() => {
             onHide()
         }, 500);
@@ -69,48 +138,96 @@ function ClienteModal(props) {
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>CPF/CNPJ</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.documento}
+                            <Col sm={10}><Form.Control type="text" defaultValue={Validar.CPF.isFormatValid(cliente.documento)?CPF.getFormated(cliente.documento):CNPJ.getFormated(cliente.documento)}
                                                        disabled={true} readOnly={true}/></Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Email</Form.Label>
-                            <Col sm={10}><Form.Control type="email" defaultValue={cliente.email}
-                                                       onChange={(e) => dados_novos.email = e.target.value}/></Col>
+                            <Col sm={10}>
+                                <Form.Control 
+                                    type="email" 
+                                    isInvalid={emailError} 
+                                    defaultValue={cliente.email}
+                                    onChange={(e) => Validar.Email.handleOnChange(e.target.value, setEmail, setEmailError)}
+                                    onKeyDown={(e) => Validar.Email.handleKeyDown(e)}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {emailError}
+                                </Form.Control.Feedback>
+                            </Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Telefone</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.telefone}
-                                                       onChange={(e) => dados_novos.telefone = e.target.value}/></Col>
+                            <Col sm={10}>
+                                <Form.Control 
+                                    type="text" 
+                                    isInvalid={telefoneError}
+                                    defaultValue={TelFixo.getFormated(cliente.telefone)}
+                                    onChange={(e) => Validar.TelFixo.handleOnChange(e.target.value, setTelefone, setTelefoneError)}
+                                    onKeyDown={(e) => Validar.TelFixo.handleKeyDown(e)}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {telefoneError}
+                                </Form.Control.Feedback>
+                            </Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Celular</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.celular}
-                                                       onChange={(e) => dados_novos.celular = e.target.value}/></Col>
+                            <Col sm={10}>
+                                <Form.Control 
+                                    type="text" 
+                                    isInvalid={celularError}
+                                    defaultValue={TelCel.getFormated(cliente.celular)}
+                                    onChange={(e) => Validar.TelCel.handleOnChange(e.target.value, setCelular, setCelularError)}
+                                    onKeyDown={(e) => Validar.TelCel.handleKeyDown(e)}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {celularError}
+                                </Form.Control.Feedback>
+                            </Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>CEP</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.cep}
-                                                       onChange={(e) => dados_novos.cep = e.target.value}/></Col>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text" 
+                                    isInvalid={cepError}
+                                    defaultValue={CEP.getFormated(cliente.cep)} 
+                                    onChange={(e) => Validar.CEP.handleOnChange(e.target.value, setCep, setCepError)}
+                                    onKeyDown={(e) => Validar.CEP.handleKeyDown(e)}
+                                />
+                                <Form.Control.Feedback type='invalid'>
+                                    {cepError}
+                                </Form.Control.Feedback>
+                            </Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Rua</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.rua}
-                                                       onChange={(e) => dados_novos.rua = e.target.value}/></Col>
+                            <Col sm={10}><Form.Control type="text" value={rua}
+                                                       onChange={(e) => setRua(e.target.value)}/></Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Número</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.numero}
-                                                       onChange={(e) => dados_novos.numero = e.target.value}/></Col>
+                            <Col sm={10}>
+                                <Form.Control 
+                                    required
+                                    min={1} 
+                                    step={1} 
+                                    type="number" 
+                                    defaultValue={cliente.numero}
+                                    onChange={(e) => dados_novos.numero = e.target.value}
+                                />
+                            </Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Bairro</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.bairro}
-                                                       onChange={(e) => dados_novos.bairro = e.target.value}/></Col>
+                            <Col sm={10}><Form.Control type="text"  value={bairro}
+                                                       onChange={(e) => setBairro(e.target.value)}/></Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Cidade</Form.Label>
-                            <Col sm={10}><Form.Control type="text" defaultValue={cliente.cidade}
-                                                       onChange={(e) => dados_novos.cidade = e.target.value}/></Col>
+                            <Col sm={10}><Form.Control type="text"  value={cidade}
+                                                       onChange={(e) => setCidade(e.target.value)}/></Col>
                         </Form.Group>
                         <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm={2}>Data Cadastro</Form.Label>
--- client/src/pages/clientes/clientes.js
+++ client/src/pages/clientes/clientes.js
@@ -6,9 +6,19 @@ import {useState} from "react";
 import {ButtonGroup, Dropdown, InputGroup} from "react-bootstrap";
 import ClienteModal from "../../components/ClienteModal";
 import searchIcon from "../../css/Icons";
+import Alert from 'react-bootstrap/Alert';
+import { 
+    CadastroPessoaFisica as CPF,
+    CadastroNacionalPessoaJuridica as CNPJ,
+    TelefoneCelular as TelCel,
+    TelefoneFixo as TelFixo,
+    CEP,
+    Data
+} from "../validacao";
 
 function ClienteBox(props) {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(false);
+    const {setMsgAlert, setShowAlert, setTypeAlert} = props;
     const {cliente} = props;
 
     const handleInativar = (cliente_id) => {
@@ -20,34 +30,67 @@ function ClienteBox(props) {
             mode: "cors",
             body: JSON.stringify({cliente_id})
         })
-            .then((resultado) => resultado.json())
-            .then((response) => {
-                if (response.success) window.location.reload();
-                else alert("Erro ao inativar Cliente");
-            })
+        .then((resultado) => resultado.json())
+        .then((response) => {
+            if (response.success){
+                if(setShowAlert && setMsgAlert && setTypeAlert){
+                    setShowAlert(true);
+                    setMsgAlert(`Cliente inativado com sucesso`);
+                    setTypeAlert("success");
+                }
+                // console.log(`Resposta: ${response.success}`);
+                setTimeout(() => {
+                    window.location.reload();  
+                }, 2000);
+            }else if(setShowAlert && setMsgAlert && setTypeAlert){
+                setShowAlert(true);
+                setMsgAlert(`Não foi possível inativar o cliente`);
+                setTypeAlert("info");
+            }
+        })
+        .catch((err) => {
+            if(setShowAlert && setMsgAlert && setTypeAlert){
+                setShowAlert(true);
+                setTypeAlert('danger');
+                if(err instanceof TypeError && err.message === "Failed to fetch")
+                    setMsgAlert(`Erro: Verifique sua conexão com a internet (${err.message}).`);
+                else
+                    setMsgAlert(`Erro: ${err.message}`);
+            }else{
+                console.error(err);
+            }
+        });
     };
 
     return (
         <div className="tecnico">
             <p key={`${cliente._id}`}>ID: {cliente._id}</p><hr/>
             <p key={`${cliente._id}_nome`}>NOME: {cliente.nome}</p><hr/>
-            <p key={`${cliente._id}_documento`}>CPF/CNPJ: {cliente.documento}</p><hr/>
+            <p key={`${cliente._id}_documento`}>CPF/CNPJ: {CPF.isFormatValid(cliente.documento)?CPF.getFormated(cliente.documento):CNPJ.getFormated(cliente.documento)}</p><hr/>
             <p key={`${cliente._id}_email`}>EMAIL: {cliente.email}</p><hr/>
-            <p key={`${cliente._id}_telefone`}>TELEFONE: {cliente.telefone}</p><hr/>
-            <p key={`${cliente._id}_celular`}>CELULAR: {cliente.celular}</p><hr/>
-            <p key={`${cliente._id}_cep`}>CEP: {cliente.cep}</p><hr/>
+            <p key={`${cliente._id}_telefone`}>TELEFONE: {TelFixo.getFormated(cliente.telefone)}</p><hr/>
+            <p key={`${cliente._id}_celular`}>CELULAR: {TelCel.getFormated(cliente.celular)}</p><hr/>
+            <p key={`${cliente._id}_cep`}>CEP: {CEP.getFormated(cliente.cep)}</p><hr/>
             <p key={`${cliente._id}_rua`}>RUA: {cliente.rua}</p><hr/>
             <p key={`${cliente._id}_bairro`}>BAIRRO: {cliente.bairro}</p><hr/>
             <p key={`${cliente._id}_numero`}>NUMERO: {cliente.numero}</p><hr/>
             <p key={`${cliente._id}_cidade`}>CIDADE: {cliente.cidade}</p><hr/>
-            <p key={`${cliente._id}_dataCriacao`}>DATA CRIAÇÃO: {cliente.dataCriacao}</p><hr/>
+            <p key={`${cliente._id}_dataCriacao`}>DATA CRIAÇÃO: {Data.getOnlyDateBRFormat(cliente.dataCriacao)}</p><hr/>
             <ButtonGroup>
                 <Button onClick={() => {setShow(true)}}>Editar</Button>
                 {cliente.bd_status !== "INATIVO" && (
                     <Button variant="danger" onClick={() => handleInativar(cliente._id)}>Inativar</Button>
                 )}
             </ButtonGroup>
-            <ClienteModal show={show} cliente={cliente} onHide={() => setShow(false)} handleClose={() => setShow(false)}/>
+            <ClienteModal 
+                show={show} 
+                cliente={cliente} 
+                onHide={() => setShow(false)} 
+                handleClose={() => setShow(false)}
+                setMsgAlert={setMsgAlert} 
+                setShowAlert={setShowAlert} 
+                setTypeAlert={setTypeAlert}
+            />
         </div>
     )
 
@@ -60,6 +103,10 @@ export default function Consultar_Clientes() {
     const [pesquisa, setPesquisa] = useState("");
     const [parametro, setParametro] = useState("nome");
     const [parametroOrd, setParametroOrd] = useState("nome");
+
+    const [showAlert, setShowAlert] = useState(false);
+    const [msgAlert, setMsgAlert] = useState('');
+    const [typeAlert, setTypeAlert] = useState('');
 
     const dropdown = (dropdown) => {
         let funcao, todos = true;
@@ -92,6 +139,14 @@ export default function Consultar_Clientes() {
 
     return (
         <div className="body-main">
+            <Alert 
+                variant={typeAlert} 
+                show={showAlert} 
+                onClose={() => setShowAlert(false)} 
+                dismissible
+            >
+                <strong><p>{msgAlert}</p></strong>
+            </Alert>
             <InputGroup className="mb-3">
                 <InputGroup.Text style={{ opacity: 0.5 }} >{searchIcon}</InputGroup.Text>
                 <Form.Control placeholder='Buscar...' onChange={e => setPesquisa(e.target.value)}/>
@@ -167,7 +222,12 @@ export default function Consultar_Clientes() {
                         }
                     }).map((cliente) => {
                         return (
-                            <ClienteBox cliente={cliente}/>
+                            <ClienteBox 
+                                cliente={cliente} 
+                                setMsgAlert={setMsgAlert} 
+                                setShowAlert={setShowAlert} 
+                                setTypeAlert={setTypeAlert}
+                            />
                         );
                     })
                 }
