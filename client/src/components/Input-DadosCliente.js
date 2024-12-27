2b591a0062f405a90d042d72331f34df31aa4d91
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-27 10:36:40 -0300

    Mudando os componentes para controlados

--- client/src/components/Input-DadosCliente.js
+++ client/src/components/Input-DadosCliente.js
@@ -1,6 +1,5 @@
 import {  Col, Row, Form, InputGroup } from 'react-bootstrap';
-import { useState } from 'react';
-import  InputCPF  from './Input-CPF';
+import InputCPF from './Input-CPF';
 import InputCNPJ from './Input-CNPJ';
 import InputNomePessoa from './Input-NomePessoa';
 import InputTelefoneFixo from './Input-TelFixo';
@@ -9,9 +8,14 @@ import InputEmail from './Input-Email';
 import ToggleCpfCnpj from './ToggleCpfCnpj';
 
 export default function InputDadosCliente({
-    docSetter, nameSetter, emailSetter, telSetter, celSetter, ...props
+    doc, docSetter,
+    name, nameSetter,
+    email, emailSetter, 
+    tel, telSetter,
+    cel, celSetter, 
+    cpfChecked, cpfCheckedSetter,
+    ...props
 }){
-    const [cpfChecked, setCpfChecked] = useState(false);
     
     return (
     <Form.Group {...props} className='border border-light-subtle p-2 rounded-1 mb-2 bg-light'>
@@ -20,32 +24,32 @@ export default function InputDadosCliente({
             <Col lg={5} xl={4}>
                 <Form.Label>Documento</Form.Label>
                 <InputGroup sm='auto'>
-                    <ToggleCpfCnpj value={cpfChecked} valueSetter={setCpfChecked}/>
+                    <ToggleCpfCnpj value={cpfChecked} valueSetter={cpfCheckedSetter}/>
                     {cpfChecked ? 
-                        <InputCPF required  valueSetter={docSetter} feedbackStyle={{paddingLeft: '110px'}}/>
+                        <InputCPF id='cpf' required value={doc}  valueSetter={docSetter} feedbackStyle={{paddingLeft: '110px'}}/>
                         :
-                        <InputCNPJ required valueSetter={docSetter} feedbackStyle={{paddingLeft: '110px'}}/>
+                        <InputCNPJ id='cnpj' required value={doc} valueSetter={docSetter} feedbackStyle={{paddingLeft: '110px'}}/>
                     }
                 </InputGroup>
             </Col>
             <Col>
                 <Form.Label>Nome Completo</Form.Label>
-                <InputNomePessoa required valueSetter={nameSetter}/>
+                <InputNomePessoa required value={name} valueSetter={nameSetter}/>
             </Col>
         </Row>
         <Form.FloatingLabel className='display-6 border-bottom border-secondary-subtle my-3'>Contatos</Form.FloatingLabel>
         <Row>
             <Col >
                 <Form.Label>Telefone Fixo</Form.Label>
-                <InputTelefoneFixo required valueSetter={telSetter}/>
+                <InputTelefoneFixo required value={tel} valueSetter={telSetter}/>
             </Col>
             <Col>
                 <Form.Label>Telefone Celular</Form.Label>
-                <InputTelefoneCelular required valueSetter={celSetter}/>
+                <InputTelefoneCelular required value={cel} valueSetter={celSetter}/>
             </Col>
             <Col sm={12} lg={6} xl={7} xxl={8}>
                 <Form.Label>Email</Form.Label>
-                <InputEmail required valueSetter={emailSetter}/>
+                <InputEmail required value={email} valueSetter={emailSetter}/>
             </Col>
         </Row>
     </Form.Group>
--- client/src/components/Input-Endereco.js
+++ client/src/components/Input-Endereco.js
@@ -1,59 +1,44 @@
 import Form from 'react-bootstrap/Form';
-import CEP from "../utils/cep";
 import InputCEP from './Input-CEP';
 import InputTextRelativeToCEP from './Input-TextRelativeToCEP';
-import { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 
 
 export default function InputEndereco({
-    cepSetter, cepValue, 
-    citySetter, cityValue, 
-    neighborhoodSetter, neighborhoodValue, 
-    streetSetter, streetValue, numberSetter, ...props
+    cepSetter, cep, cepErrorSetter, 
+    citySetter, city, 
+    neighborhoodSetter, neighborhood, 
+    streetSetter, street, 
+    numberSetter, number, ...props
 }){
-
-    useEffect(() => {
-        const updateData = async function(){
-            try{
-                const data = await CEP.getDataFrom(cepValue);
-                if(data){
-                    citySetter(data.estado ?? '');
-                    neighborhoodSetter(data.bairro ?? '');
-                    streetSetter(data.logradouro ?? '');
-                }
-            }catch(error){
-                console.error(error);
-            }
-        }
-        if(CEP.isFormatValid(cepValue))
-            updateData();
-    }, [cepValue])
 
     return <Form.Group {...props} className='border border-light-subtle p-2 rounded-1 mb-2 bg-light'>
         <Form.FloatingLabel className='display-4 border-bottom border-primary-subtle mb-3'>Endereço</Form.FloatingLabel>
         <Form.Label>CEP</Form.Label>
         <InputCEP
+            required
+            value={cep}
             valueSetter={cepSetter}
+            msgErrorSetter={cepErrorSetter}
         />
         <Row>
             <Col md={6}>
                 <Form.Label>Cidade</Form.Label>
-                <InputTextRelativeToCEP valueSetter={citySetter}/>
+                <InputTextRelativeToCEP required value={city} valueSetter={citySetter}/>
             </Col>
             <Col md={6}>
                 <Form.Label>Bairro</Form.Label>
-                <InputTextRelativeToCEP valueSetter={neighborhoodSetter}/>
+                <InputTextRelativeToCEP required value={neighborhood} valueSetter={neighborhoodSetter}/>
             </Col>
         </Row>
         <Row>
             <Col md={8}>
                 <Form.Label>Rua</Form.Label>
-                <InputTextRelativeToCEP valueSetter={streetSetter}/>
+                <InputTextRelativeToCEP required value={street} valueSetter={streetSetter}/>
             </Col>
             <Col md={4}>
                 <Form.Label>Número</Form.Label>
-                <InputTextRelativeToCEP type='number' valueSetter={numberSetter}/>
+                <InputTextRelativeToCEP required value={number} type='number' valueSetter={numberSetter}/>
             </Col>
         </Row>
         
