49c85543354848454c3b4986c248169745274a13
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-12-27 10:35:25 -0300

    Consertando prop type da Texto relativo ao cep

--- client/src/components/Input-TextRelativeToCEP.js
+++ client/src/components/Input-TextRelativeToCEP.js
@@ -2,14 +2,14 @@ import Form from "react-bootstrap/Form";
 import NonEmptyField from '../utils/non-emptyfield'
 import { useState } from "react";
 
-export default function InputTextRelativeToCEP({value, valueSetter, msgCepError, msgError, msgErrorSetter, defaultValue, type, readOnly, ...props}){
+export default function InputTextRelativeToCEP({valueSetter, msgCepError, msgError, msgErrorSetter, defaultValue, type, readOnly, ...props}){
     const [erro, setErro] = useState('');
 
     return (
         <>
             <Form.Control
                 {...props}
-                type={type}
+                type={type === undefined ? 'text' : type}
                 min={type === 'number' ? 1 : undefined} 
                 step={type === 'number' ? 1 : undefined}
                 readOnly={type === 'number' ? false : (readOnly || !(msgCepError && msgCepError !== "Obrigatório!" && msgCepError !== "Incompleto!"))} 
